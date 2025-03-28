373b0b2fea6554887c92474466ed59f8bd55a674
Kacper Krupa <s30781@pjwstk.edu.pl>
2025-03-28 14:42:44 +0100

    zaczalem zmieniac kod doalem metody sprawdzajace imie naziwsko wiek i email

--- zadanie/zadanie/LegacyApp/UserService.cs
+++ zadanie/zadanie/LegacyApp/UserService.cs
@@ -6,21 +6,18 @@ namespace LegacyApp
     {
         public bool AddUser(string firstName, string lastName, string email, DateTime dateOfBirth, int clientId)
         {
-            if (string.IsNullOrEmpty(firstName) || string.IsNullOrEmpty(lastName))
+            //sprawdzenie czy imie jest nie puste
+            if (isNameEmpty(firstName, lastName) == false)
             {
                 return false;
             }
-
-            if (!email.Contains("@") && !email.Contains("."))
+            //sprawdzenie maila
+            if (isEmailCorrect(email) == false)
             {
                 return false;
             }
-
-            var now = DateTime.Now;
-            int age = now.Year - dateOfBirth.Year;
-            if (now.Month < dateOfBirth.Month || (now.Month == dateOfBirth.Month && now.Day < dateOfBirth.Day)) age--;
-
-            if (age < 21)
+            //sprawdzenie daty urodzenia
+            if (isOldEnough(dateOfBirth) == false)
             {
                 return false;
             }
@@ -67,6 +64,39 @@ namespace LegacyApp
 
             UserDataAccess.AddUser(user);
             return true;
+        }
+
+        public bool isNameEmpty(String firstName, String lastName)
+        {
+            if (string.IsNullOrEmpty(firstName) || string.IsNullOrEmpty(lastName))
+            {
+                return false;
+            }else return true;
+        }
+
+        public bool isEmailCorrect(string email)
+        {
+            if (!email.Contains("@") && !email.Contains("."))
+            {
+                return false;
+            }
+            else return true;
+        }
+
+        public bool isOldEnough(DateTime dateOfBirth)
+        {
+            var now = DateTime.Now;
+            int age = now.Year - dateOfBirth.Year;
+            if (now.Month < dateOfBirth.Month || (now.Month == dateOfBirth.Month && now.Day < dateOfBirth.Day)) age--;
+
+            if (age < 21)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
         }
     }
 }
